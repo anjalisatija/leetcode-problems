f79ab8d9f04763893d2c88e5c7d9841b838701c4
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-08-28 23:24:03 +0530

    Time: 1 ms (48.85%), Space: 42.7 MB (65.77%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n= nums.length;
+        int power= 1<<n;
+        List<List<Integer>> res= new ArrayList<>();
+        
+        for(int i=0;i<power;i++){
+            List<Integer> temp= new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i & (1<<j)) !=0){
+                    temp.add(nums[j]);
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+}
