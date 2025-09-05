cac28a7ddf6cdb3f0e7c4a546ab5c51da462f749
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-09-05 23:21:33 +0530

    Time: 0 ms (100%), Space: 16.9 MB (61.22%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    static bool comp(string &a, string &b)
+    {
+        return a + b > b + a;
+    }
+    
+    string largestNumber(vector<int>& nums)
+    {
+        vector<string> numStrings;
+        for (int num : nums)
+            numStrings.push_back(to_string(num));
+        sort(numStrings.begin(), numStrings.end(), comp);
+        if (numStrings[0] == "0")
+            return "0";
+        string result;
+        for (string numStr : numStrings)
+            result += numStr;        
+        return result;
+    }
+};
