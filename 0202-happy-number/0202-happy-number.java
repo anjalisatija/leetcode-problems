d7cf4a261cbbde07aedde23cf73ea5e0df580e45
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-09-09 03:17:05 +0530

    Time: 1 ms (63.35%), Space: 41.3 MB (13.49%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set =new HashSet();
+        while(!set.contains(n))
+        {
+            set.add(n);
+            int sum=0;
+            while(n>0)
+            {
+                sum+=Math.pow(n%10,2);
+                n=n/10;
+            }
+            if(sum==1)
+            {
+                return true;
+            }
+            n=sum;
+        }
+        return false;
+    }
+}
