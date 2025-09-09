08b5e3cc8ffa81105406fbfd434a414ae05086a9
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-09-10 00:31:12 +0530

    Time: 68 ms (84.99%), Space: 98.8 MB (15.85%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int n = nums[i];
+
+            if (m.find(n) != m.end() && abs(i - m[n]) <= k) {
+                return true;
+            }
+
+            m[n] = i;
+        }
+
+        return false;
+    }
+};
