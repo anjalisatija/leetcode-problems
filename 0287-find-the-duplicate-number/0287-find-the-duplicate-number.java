8c5e5f0e369dd931e19989cc95dd5290f205a540
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-09-14 23:50:35 +0530

    Time: 22 ms (26.45%), Space: 56.9 MB (93.9%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;  
+            }
+            set.add(num);
+        }
+        return -1; 
+    }
+}
