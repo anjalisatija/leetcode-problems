e1cdc46bf61f22d930c7213f7877711386c66729
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-09-19 05:35:57 +0530

    Time: 4 ms (64.35%), Space: 55.1 MB (44.93%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.java
@@ -0,0 +1,22 @@
+class Solution {
+    private boolean check(int[] d) {
+        if(d.length < 4) return false;
+        for(int i = 0; i < d.length; i++) {
+            if(i >= 3 && d[i] >= d[i-2] && d[i-1] <= d[i-3]) {
+                return true;
+            }
+            if(i >= 4 && d[i-1] == d[i-3] && d[i] + d[i-4] >= d[i-2]) {
+                return true;
+            }
+            if(i >= 5 && d[i] <= d[i-2] && d[i] + d[i-4] >= d[i-2] && 
+            d[i-1] <= d[i-3] && d[i-1] + d[i-5] >= d[i-3] && 
+            d[i-2] >= d[i-4]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isSelfCrossing(int[] distance) {
+        return check(distance);
+    }
+}
