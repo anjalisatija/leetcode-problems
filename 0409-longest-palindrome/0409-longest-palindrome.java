7e11748b3cc2056584e8b512404496e5567b647c
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-09-22 22:13:00 +0530

    Time: 7 ms (54.58%), Space: 42.4 MB (13.68%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(set.contains(s.charAt(i)))
+            {
+                set.remove(s.charAt(i));
+                count+=2;
+            }
+            else {set.add(s.charAt(i));}  
+        }
+        return count+(set.isEmpty()?0:1);
+    }
+}
