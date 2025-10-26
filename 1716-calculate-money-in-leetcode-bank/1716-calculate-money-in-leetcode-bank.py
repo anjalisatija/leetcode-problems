20dcf9250c6888005356be3aeb24e2154e5abaf0
ANJALI SATIJA <anjalisatija11@gmail.com>
2025-10-26 18:37:09 +0530

    Time: 0 ms (100%), Space: 17.9 MB (15.71%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,6 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        weeks, days = divmod(n, 7)
+        total = 28 * weeks + 7 * weeks * (weeks - 1) // 2
+        total += days * (days + 1) // 2 + weeks * days
+        return total
